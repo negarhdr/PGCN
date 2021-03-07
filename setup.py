2f0e3641116d65dfeabfeeb3dadd40915319e7bc
Negar Heidari <36771997+negarhdr@users.noreply.github.com>
2021-03-07 20:16:48 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='PGCN',
+      version='0.1',
+      description='Progressive Graph Convolutional Networks for Semi-Supervised Node Classification in PyTorch',
+      author='Negar Heidari',
+      author_email='negar.heidari@ece.au.dk',
+      download_url='https://github.com/tkipf/pygcn',
+      license='MIT',
+      install_requires=['numpy',
+                        'torch',
+                        'scipy'
+                        ],
+      package_data={'pygcn': ['README.md']},
+      packages=find_packages())
